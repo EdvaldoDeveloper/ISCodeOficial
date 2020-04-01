f6dc14a06acb716d99ebec8fb150733e757dafa5
Edvaldo Silva <edvaldodeveloper@gmail.com>
2020-04-01 01:46:09 -0300

    Deploy

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const routeIndex = require('./app/routes/index')(app);
 const routeMassege = require('./app/routes/formMessage')(app);
 
 
-app.listen(8000, () => {
+app.listen(3000, () => {
     console.log("Servidor ON");
 });
